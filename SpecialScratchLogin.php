dd752f1187f49c145b97f74bf6214a72235235df
Kenny2github <kenny2minecraft@gmail.com>
2020-08-09 19:57:09 +0800

    Cleanup
    * define() API and project URLs
    * wfMessage replaces all hardcoded text
    
    sessionVerificationCode:
    * Use sha256 on cryptographic random instead of sha1 on rand()
    
    getComments:
    * Retrieve project author and ID with wfMessage, and sprintf
      them into the define()d API_URL
    - Remove $project_id parameter
    * Returned JSON is in assoc array form
    
    commenter:
    - Remove $project_id parameter
    - Don't pass $project_id into getComments()
    * Returned JSON is now in assoc array form
    * Prefer if over ?:
    
    +SpecialScratchLogin::getGroupName:
    + This special page is in the login group.
    
    SpecialScratchLogin::execute:
    * Make whitespace consistent at least within this function
    
    SpecialScratchLogin::loginForm:
    * Construct and wfMessage the link string
    
    SpecialScratchLogin::showError:
    * Construct the paragraph
    * Give it class=error
    
    SpecialScratchLogin::onPost:
    - Don't pass empty $project_id
    
    --- other files ---
    extension.json:
    + Add myself as author
    * Change url from default
    
    en.json:
    * Made scratchlogin-desc, title, summary
      more appropriate to their purpose
    + scratchlogin-project-id, author specify the author and ID
      of the verification project
    + scratchlogin-instructions: instructions shown on special page
    + scratchlogin-uncommented: error message for no code
    + scratchlogin-unregistered: error message for no such user
    + scratchlogin-success: success message
    
    qqq.json:
    * Documented en.json

--- SpecialScratchLogin.php
+++ SpecialScratchLogin.php
@@ -1,31 +1,44 @@
 <?php
+define('API_URL', 'https://api.scratch.mit.edu/users/%s/projects/%s/comments?offset=0&limit=20');
+define('PROJECT_LINK', 'https://scratch.mit.edu/projects/%s/');
+
 function sessionVerificationCode(&$session) {
 	if ($session->exists('vercode')) {
 		return $session->get('vercode');
 	} else {
 		$session->persist();
-		$session->set('vercode', sha1(rand()));
+		$session->set('vercode', hash('sha256', random_bytes(16)));
 		$session->save();
 		return $session->get('vercode');
 	}
 }
 
-function getComments($project_id) {
-	return json_decode(file_get_contents('https://api.scratch.mit.edu/users/ModShare/projects/10135908/comments?offset=0&limit=20'));
+function getComments() {
+	return json_decode(file_get_contents(sprintf(
+		API_URL, wfMessage('scratchlogin-project-author')->text(),
+		wfMessage('scratchlogin-project-id')->text()
+	)), TRUE);
 }
 
-function commenter($project_id, $req_comment) {
-	$comments = getComments($project_id);
+function commenter($req_comment) {
+	$comments = getComments();
 	
-	$matching_comments = array_filter($comments, function(&$comment) use($req_comment) { return stristr((string)$comment->content, (string)$req_comment); });
-	return empty($matching_comments) ?
-		null :
-		(string)$matching_comments[0]->author->username;
+	$matching_comments = array_filter($comments, function(&$comment) use($req_comment) {
+		return stristr($comment['content'], $req_comment);
+	});
+	if (empty($matching_comments)) {
+		return null;
+	}
+	return $matching_comments[0]['author']['username'];
 }
 
 class SpecialScratchLogin extends SpecialPage {
 	function __construct() {
 		parent::__construct('ScratchLogin');
+	}
+	
+	function getGroupName() {
+		return 'login';
 	}
 	
 	function execute($par) {
@@ -36,7 +49,7 @@ class SpecialScratchLogin extends SpecialPage {
 		if ($request->wasPosted()) {
 			self::onPost( $par, $out, $request );
 		} else {
-			self::loginForm($par, $out, $request);
+			self::loginForm( $par, $out, $request );
 		}
 	}
 	
@@ -46,14 +59,23 @@ class SpecialScratchLogin extends SpecialPage {
 				[ 'method' => 'POST' ]
 		));
 		
-		$out->addHTML('<p>You need to enter the following verification code on the <a href="https://scratch.mit.edu/projects/10135908/" target="_blank">user verification project</a>: <br /><b>' . sessionVerificationCode($request->getSession()) . '</b></p>');
+		$link = Html::openElement('a', [
+			'href' => sprintf(PROJECT_LINK, wfMessage('scratchlogin-project-id')->text()),
+			'target' => '_blank'
+		]);
+		
+		$out->addHTML(wfMessage(
+			'scratchlogin-instructions',
+			$link, Html::closeElement( 'a' ),
+			sessionVerificationCode($request->getSession())
+		)->inContentLanguage()->parseAsBlock());
 				
 		$out->addHTML(Html::rawElement(
 			'input',
 			[
 				'type' => 'submit',
 				'id' => 'mw-scratchlogin-form-submit',
-				'value' => 'Log in'
+				'value' => wfMessage('login')->inContentLangauge()->plain()
 			]
 		));
 		
@@ -61,23 +83,31 @@ class SpecialScratchLogin extends SpecialPage {
 	}
 	
 	private static function showError($error, $par, $out, $request) {
-		$out->addHTML('<p>' . $error . '</p>');
+		$out->addHTML(Html::rawElement('p', [ 'class' => 'error' ], $error));
 		self::loginForm($par, $out, $request);
 	}
 	
 	private static function onPost( $par, $out, $request ) {
 		global $wgSecureLogin;
 		
-		$username = commenter('', sessionVerificationCode($request->getSession()));
+		$username = commenter(sessionVerificationCode($request->getSession()));
 		
 		if ($username == null) {
-			self::showError('You do not appear to have comented the code on the project.', $par, $out, $request);
+			self::showError(
+				wfMessage('scratchlogin-uncommented')
+				->inContentLangauge()->plain(),
+				$par, $out, $request
+			);
 			return;
 		}
 						
 		$user = User::newFromName($username);
 		if ($user->getId() == 0) {
-			self::showError('The user <b>' . $username . '</b> is not registered on this wiki', $par, $out, $request);
+			self::showError(
+				wfMessage('scratchlogin-unregistered', $username)
+				->inContentLangauge()->parse(),
+				$par, $out, $request
+			);
 			return;
 		}
 		
@@ -85,6 +115,6 @@ class SpecialScratchLogin extends SpecialPage {
 		$request->getSession()->setUser($user);
 		$request->getSession()->save();
 		
-		$out->addHTML('<p>Success! You are now logged in as <b>' . $username . '</b></p>');
+		$out->addWikiMsg('scratchlogin-success', $username);
 	}
 }
