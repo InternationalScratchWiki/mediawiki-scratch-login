e3f334b961bb882ecd7c02d864193ad256c6245d
Kenny2github <kenny2minecraft@gmail.com>
2020-08-09 13:34:45 +0100

    Fixes after testing
    * Rid files of trailing whitespace
    
    +setCode:
    + Do the verification code generation and setting here
    
    sessionVerificationCode:
    * Reduce duplication:
      1. If the vercode doesn't exist, set it with setCode
      2. Return the vercode
    
    execute:
    + Allow resetting verification code by going to Special:ScratchLogin/reset
    
    loginForm:
    * Fix: pass <a> elements as raw params so they don't get escaped
    * Fix: Langauge->Language
    
    onPost:
    - $wgSecureLogin isn't used anywhere, don't global it
    * Fix: Langauge->Language
    
    +resetCode:
    * Call setCode then link back to root special page
    
    --- other files ---
    en.json:
    * scratchlogin-instructions: Add line linking to reset-code page
    * scratchlogin-success: Make more accurate based on my experience
    + scratchlogin-code-reset: Message after code reset links to root page
    
    qqq.json:
    + Document new string

--- SpecialScratchLogin.php
+++ SpecialScratchLogin.php
@@ -2,15 +2,17 @@
 define('API_URL', 'https://api.scratch.mit.edu/users/%s/projects/%s/comments?offset=0&limit=20');
 define('PROJECT_LINK', 'https://scratch.mit.edu/projects/%s/');
 
+function setCode(&$session) {
+	$session->persist();
+	$session->set('vercode', strtr(hash('sha256', random_bytes(16)), '0123456789', 'ABCDEFGHIJ'));
+	$session->save();
+}
+
 function sessionVerificationCode(&$session) {
-	if ($session->exists('vercode')) {
-		return $session->get('vercode');
-	} else {
-		$session->persist();
-		$session->set('vercode', hash('sha256', random_bytes(16)));
-		$session->save();
-		return $session->get('vercode');
+	if (!$session->exists('vercode')) {
+		setCode($session);
 	}
+	return $session->get('vercode');
 }
 
 function getComments() {
@@ -22,7 +24,7 @@ function getComments() {
 
 function commenter($req_comment) {
 	$comments = getComments();
-	
+
 	$matching_comments = array_filter($comments, function(&$comment) use($req_comment) {
 		return stristr($comment['content'], $req_comment);
 	});
@@ -36,85 +38,89 @@ class SpecialScratchLogin extends SpecialPage {
 	function __construct() {
 		parent::__construct('ScratchLogin');
 	}
-	
+
 	function getGroupName() {
 		return 'login';
 	}
-	
+
 	function execute($par) {
 		$request = $this->getRequest();
 		$out = $this->getOutput();
 		$this->setHeaders();
-		
-		if ($request->wasPosted()) {
+
+		if ($par == 'reset') {
+			self::resetCode( $par, $out, $request );
+		} else if ($request->wasPosted()) {
 			self::onPost( $par, $out, $request );
 		} else {
 			self::loginForm( $par, $out, $request );
 		}
 	}
-	
+
 	static private function loginForm($par, $out, $request) {
 		$out->addHTML(Html::openElement(
 				'form',
 				[ 'method' => 'POST' ]
 		));
-		
+
 		$link = Html::openElement('a', [
 			'href' => sprintf(PROJECT_LINK, wfMessage('scratchlogin-project-id')->text()),
 			'target' => '_blank'
 		]);
-		
-		$out->addHTML(wfMessage(
-			'scratchlogin-instructions',
+
+		$out->addHTML(wfMessage('scratchlogin-instructions')->rawParams(
 			$link, Html::closeElement( 'a' ),
 			sessionVerificationCode($request->getSession())
 		)->inContentLanguage()->parseAsBlock());
-				
+
 		$out->addHTML(Html::rawElement(
 			'input',
 			[
 				'type' => 'submit',
 				'id' => 'mw-scratchlogin-form-submit',
-				'value' => wfMessage('login')->inContentLangauge()->plain()
+				'value' => wfMessage('login')->inContentLanguage()->plain()
 			]
 		));
-		
+
 		$out->addHTML(Html::closeElement( 'form' ));
 	}
-	
+
 	private static function showError($error, $par, $out, $request) {
 		$out->addHTML(Html::rawElement('p', [ 'class' => 'error' ], $error));
 		self::loginForm($par, $out, $request);
 	}
-	
+
 	private static function onPost( $par, $out, $request ) {
-		global $wgSecureLogin;
-		
 		$username = commenter(sessionVerificationCode($request->getSession()));
-		
+
 		if ($username == null) {
 			self::showError(
 				wfMessage('scratchlogin-uncommented')
-				->inContentLangauge()->plain(),
+				->inContentLanguage()->plain(),
 				$par, $out, $request
 			);
 			return;
 		}
-						
+
 		$user = User::newFromName($username);
 		if ($user->getId() == 0) {
 			self::showError(
 				wfMessage('scratchlogin-unregistered', $username)
-				->inContentLangauge()->parse(),
+				->inContentLanguage()->parse(),
 				$par, $out, $request
 			);
 			return;
 		}
-		
+
 		$request->getSession()->clear('vercode');
 		$request->getSession()->setUser($user);
 		$request->getSession()->save();
-		
+
 		$out->addWikiMsg('scratchlogin-success', $username);
+	}
+
+	private static function resetCode( $par, $out, $request) {
+		setCode($request->getSession());
+		$out->addWikiMsg('scratchlogin-code-reset');
 	}
 }
